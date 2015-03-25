6829ec5252768501dce40e9c5bdba95c74ba98e8
Marius van Eck <marius.v.eck@gmail.com>
2015-03-25 14:57:53 +0100

    loop over beta

--- h2_mh.f90
+++ h2_mh.f90
@@ -1,14 +1,15 @@
 
-SUBROUTINE metropolis_hastings(s, a, b, energy, plot_bin)
+SUBROUTINE metropolis_hastings(s, a, b, num_bins, energy, plot_bin)
     implicit none
 
     real(8), intent(in) :: s, a, b
+    integer, intent(in) :: num_bins
     real(8), intent(out) :: energy
-    integer(8), intent(out) :: plot_bin(0:49,0:49)
+    integer(8), intent(out) :: plot_bin(num_bins,num_bins)
     
     integer :: i, bi_x, bi_y, N
     real(8) :: r1(0:2), r2(0:2), r1n(0:2), r2n(0:2), t(0:2), temp(0:2)
-    real(8) :: rnd(1:9)
+    real(8) :: rnd(1:7)
     real(8) :: PI, p
     
     real(8) :: r1L, r1R, r2L, r2R, r12, r12n, p1L, p1R, p2L, p2R, p12
@@ -42,10 +43,10 @@ SUBROUTINE metropolis_hastings(s, a, b, energy, plot_bin)
                 sin(PI*rnd(3))*sin(2*PI*rnd(4)), &                  !  sin(theta)sin(phi),
                 cos(PI*rnd(3)) /)                                   !  cos(theta))
 
-        r2n = r2 + (s/2)*sqrt(-2*log(rnd(5)))*cos(2*PI*rnd(6))* &
-            (/  sin(PI*rnd(7))*cos(2*PI*rnd(8)), &
-                sin(PI*rnd(7))*sin(2*PI*rnd(8)), &
-                cos(PI*rnd(7)) /)
+        r2n = r2 + (s/2)*sqrt(-2*log(rnd(1)))*sin(2*PI*rnd(2))* &
+            (/  sin(PI*rnd(5))*cos(2*PI*rnd(6)), &
+                sin(PI*rnd(5))*sin(2*PI*rnd(6)), &
+                cos(PI*rnd(5)) /)
 
         ! calculate the fraction of the new wave function divided by the old
         ! one
@@ -59,7 +60,7 @@ SUBROUTINE metropolis_hastings(s, a, b, energy, plot_bin)
         
 
         ! update r1 and r2 according to metropolis hastings algorithm
-        IF (p**2 > rnd(9)) THEN
+        IF (p**2 > rnd(7)) THEN
             r1 = r1n
             r2 = r2n
         END IF
@@ -81,21 +82,21 @@ SUBROUTINE metropolis_hastings(s, a, b, energy, plot_bin)
                 sin(PI*rnd(3))*sin(2*PI*rnd(4)), &                  !  sin(theta)sin(phi),
                 cos(PI*rnd(3)) /)                                   !  cos(theta))
 
-        r2n = r2 + (s/2)*sqrt(-2*log(rnd(5)))*cos(2*PI*rnd(6))* &
-            (/  sin(PI*rnd(7))*cos(2*PI*rnd(8)), &
-                sin(PI*rnd(7))*sin(2*PI*rnd(8)), &
-                cos(PI*rnd(7)) /)
+        r2n = r2 + (s/2)*sqrt(-2*log(rnd(1)))*sin(2*PI*rnd(2))* &
+            (/  sin(PI*rnd(5))*cos(2*PI*rnd(6)), &
+                sin(PI*rnd(5))*sin(2*PI*rnd(6)), &
+                cos(PI*rnd(5)) /)
 
         r1L = sum((r1+t)**2)**0.5
         r1R = sum((r1-t)**2)**0.5
         r2L = sum((r2+t)**2)**0.5
         r2R = sum((r2-t)**2)**0.5
         r12 = sum((r1-r2)**2)**0.5
-        r1Lu = (r1+t)/sum((r1+t)**2)**0.5
-        r1Ru = (r1-t)/sum((r1-t)**2)**0.5
-        r2Lu = (r2+t)/sum((r2+t)**2)**0.5
-        r2Ru = (r2-t)/sum((r2-t)**2)**0.5
-        r12u = (r1-r2)/sum((r1-r2)**2)**0.5
+        r1Lu = (r1+t)/r1L
+        r1Ru = (r1-t)/r1R
+        r2Lu = (r2+t)/r2L
+        r2Ru = (r2-t)/r2R
+        r12u = (r1-r2)/r12
         p1L = exp(-r1L/a)
         p1R = exp(-r1R/a)
         p2L = exp(-r2L/a)
@@ -116,21 +117,21 @@ SUBROUTINE metropolis_hastings(s, a, b, energy, plot_bin)
         energy = energy - 1/(a**2) + (p1L/r1L + p1R/r1R)/(a*(p1L+p1R)) + &
             (p2L/r2L + p2R/r2R)/(a*(p2L+p2R)) & 
             - 1/r1L - 1/r1R - 1/r2L - 1/r2R + 1/r12 + &
-            dot_product(temp, r12u/(2*a*(1-b*r12)**2)) &
+            dot_product(temp, r12u/(2*a*(1+b*r12)**2)) &
             - ((4*b+1)*r12+4)/((4*(1+b*r12)**4)*r12)
 
         ! update r1 and r2 according to metropolis hastings algorithm
-        IF (p**2 > rnd(9)) THEN
+        IF (p**2 > rnd(7)) THEN
             r1 = r1n
             r2 = r2n
         END IF
         
         ! bin the values for plotting
-        bi_x = int(50*(r2(0)+s)/(2*s))
-        bi_y = int(50*(r2(1)+s)/(2*s))
+        bi_x = int(num_bins*(r2(0)+s)/(2*s))
+        bi_y = int(num_bins*(r2(1)+s)/(2*s))
         IF (((bi_x >=0 ) .and. (bi_x < 50)) .and. &
             ((bi_y >=0 ) .and. (bi_y < 50)) .and. &
-             abs(r2(2)) < 2*s/25)  THEN
+             abs(r2(2)) < s/num_bins)  THEN
             plot_bin(bi_x,bi_y) = plot_bin(bi_x,bi_y) + 1
         END IF
     END DO
--- run.py
+++ run.py
@@ -7,20 +7,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-t = time.time()
-
+# parameter 1 (s): is the separation between the protons
 # parameter 2 (a): is a trial wave function parameter
+#                  following from a*(1+exp(-s/a))=1
 # parameter 3 (beta): is a trial wave function parameter
-# parameter 1 (s): is the seperation between the protons
-#                   following from a*(1+exp(-s/a))=1
-energy, plot_bin = h2_mh.metropolis_hastings(2.0, 0.90182909, 0.001)
-print energy
+beta = np.linspace(1.5,5.0,8)
 
-print time.time() - t
+for b in beta:
+    energy, plot_bin = h2_mh.metropolis_hastings(2.0, 0.90182909, b, 50)
+    print energy
 
-X = np.array([np.linspace(-2,2,50),]*50)
-Y = np.array([np.linspace(-2,2,50),]*50).transpose()
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.plot_wireframe(X, Y, plot_bin)
+plt.figure()
+plt.plot(beta, energy)
 plt.show()
+
+# plot
+# X = np.array([np.linspace(-2,2,50),]*50)
+# Y = np.array([np.linspace(-2,2,50),]*50).transpose()
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+# ax.plot_wireframe(X, Y, plot_bin)
+# plt.show()
